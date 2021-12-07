260b32fa57d5dafb4191c81c984220793b487524
Nemo <63228128+xNEMOx@users.noreply.github.com>
2021-12-07 11:27:15 +0400

    Add files via upload

---
+++ pass_generator.py
@@ -0,0 +1,56 @@
+import random
+import time
+import enlighten
+
+start_msg = """
+
+╭━━━┳━━━┳━━━┳━━━┳╮╭╮╭┳━━━┳━━━┳━━━╮
+┃╭━╮┃╭━╮┃╭━╮┃╭━╮┃┃┃┃┃┃╭━╮┃╭━╮┣╮╭╮┃
+┃╰━╯┃┃╱┃┃╰━━┫╰━━┫┃┃┃┃┃┃╱┃┃╰━╯┃┃┃┃┃
+┃╭━━┫╰━╯┣━━╮┣━━╮┃╰╯╰╯┃┃╱┃┃╭╮╭╯┃┃┃┃
+┃┃╱╱┃╭━╮┃╰━╯┃╰━╯┣╮╭╮╭┫╰━╯┃┃┃╰┳╯╰╯┃
+╰╯╱╱╰╯╱╰┻━━━┻━━━╯╰╯╰╯╰━━━┻╯╰━┻━━━╯
+Generator By: Nemo
+Discord: Nemo#1819
+Github: xNEMOx
+"""
+
+print(start_msg)
+pas_letters = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@']
+
+tlmn = input('Type F to generate password: ')
+tlmns = input('How many digits: ')
+
+if tlmn == 'f' or 'F':
+    pbar = enlighten.Counter(total=100, desc='Generating', unit='ticks')
+    for num in range(100):
+        time.sleep(0.03)  # Simulate work
+        pbar.update()
+    print("\033[1m" + 'Choosing numbers [             0%]' + "\033[0m")
+    time.sleep(0.1)
+    print("\033[1m" + 'Choosing numbers [=           10%]' + "\033[0m")
+    time.sleep(0.1)
+    print("\033[1m" + 'Choosing numbers [==          20%]' + "\033[0m")
+    time.sleep(0.1)
+    print("\033[1m" + 'Choosing letters [===         30%]' + "\033[0m")
+    time.sleep(0.1)
+    print("\033[1m" + 'Choosing letters [====        40%]' + "\033[0m")
+    time.sleep(0.1)
+    print("\033[1m" + 'Choosing numbers [=====       50%]' + "\033[0m")
+    time.sleep(0.1)
+    print("\033[1m" + 'Choosing numbers [======      60%]' + "\033[0m")
+    time.sleep(0.1)
+    print("\033[1m" + 'Choosing numbers [=======     70%]' + "\033[0m")
+    time.sleep(0.1)
+    print("\033[1m" + 'Completing w8w8w [========    80%]' + "\033[0m")
+    time.sleep(0.1)
+    print("\033[1m" + 'Nearly done wait [=========   90%]' + "\033[0m")
+    time.sleep(0.1)
+    print("\033[1m" + 'Success DONE!    [========== 100%]' + "\033[0m")
+    time.sleep(0.1)
+    print(''.join(random.sample(pas_letters, int(tlmns))))
+
+
+    # python3 /Users/neymatgasimli/Desktop/Python-BlockChain/pass_generator.py
+
+    
